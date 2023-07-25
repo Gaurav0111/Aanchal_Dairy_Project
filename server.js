7838b2aab24e588eddb948b8e41141165d549a63
Gaurav0111 <garwalgaurav1@gmail.com>
2023-07-25 12:44:25 +0530

    connected databse

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import dotenv from 'dotenv'
 dotenv.config()
+import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleWare from "./middleware/error-handler.js";
 
@@ -16,7 +17,17 @@ app.use(notFoundMiddleware) // if none of teh app.get() request will be matched,
 app.use(errorHandlerMiddleWare) // if none of teh app.get() request will be matched, this one will be executed
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, ()=>{
     console.log(`Server is running...${port}`)
 })
+
+const start = async ()=> {
+    try{
+        await connectDB(process.env.MONGO_URL)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+start()

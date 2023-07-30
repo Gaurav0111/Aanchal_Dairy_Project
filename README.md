53f01e6d19a79d3aa937668754ad4e00a018b65b
Gaurav0111 <garwalgaurav1@gmail.com>
2023-07-30 14:08:30 +0530

    updated README.md file

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # Aanchal_Dairy_Project
 Getting started with it on 23-07-2023
-
-
-
-28-08-2023: Still working on...
-
-for any further query kindly contact
-email: garwalgaurav480@gmail.com 
--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const app = express();
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from "./db/connect.js";
+
+import authRouter from './routes/authRoutes.js'
+
+
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleWare from "./middleware/error-handler.js";
 
-
+app.use(express.json()) // will make all the json data avaible to all controllers
 
 app.get('/',(req, res)=>{
     // throw new Error('sd')
     res.send('Welcome.. !')
 })
 
-app.use(notFoundMiddleware) // if none of teh app.get() request will be matched, this one will be executed
+app.use('api/v1/auth', authRouter)
+
+app.use(notFoundMiddleware) // if none of the app.get() request will be matched, this one will be executed
 app.use(errorHandlerMiddleWare) // if none of teh app.get() request will be matched, this one will be executed
 
 const port = process.env.PORT || 5000;

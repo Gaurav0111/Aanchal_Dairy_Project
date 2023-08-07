060d5965ba3b907ff9463d37d7c3de620da45ff7
Gaurav0111 <garwalgaurav1@gmail.com>
2023-08-07 20:34:48 +0530

    sending user data to database

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,13 +1,20 @@
+import User from "../models/User-model.js";
+
 const register = async (req, res) => {
-    res.send('Register User..')
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ user }); // Using res.status(201) and res.json()
+    } catch (error) {
+        res.status(500).json({ msg: 'There was an error while registering' });
+    }
 }
 
 const login = async (req, res) => {
-    res.send('Login User..')
+    res.send('Login User..');
 }
 
 const updateUser = async (req, res) => {
-    res.send('Update User..')
+    res.send('Update User..');
 }
 
-export {register, login, updateUser};
+export { register, login, updateUser };

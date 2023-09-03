1b19f406a04439eb98bdb008ea170af9af03067e
Gaurav0111 <garwalgaurav1@gmail.com>
2023-09-03 22:19:33 +0530

    handling errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,17 @@
+import { StatusCodes } from "http-status-codes";
 const errorHandlerMiddleWare = (err, req, res, next) => {
-    // console.log(err)
-    res.status(505).json({ msg: err })
+    console.log(err)
+    const defaultError = {
+        statusCode : StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'Something went wrong, tyr again later',
+    }
+    if (err.name==='ValidationError'){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // defaultError.msg=err.message
+        defaultError.msg = Object.values(err.errors).map((item)=> item.message).join(',')
+    }
+    // res.status(defaultError.statusCode).json({ msg: err })
+    res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleWare;

bc4a3a4fb57319cc2f265784f85510dc29c2cebe
Gaurav0111 <garwalgaurav1@gmail.com>
2023-09-04 12:28:08 +0530

    configuring different errors

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,19 @@
 import User from "../models/User-model.js";
+import { StatusCodes } from "http-status-codes";
+import {BadRequestError} from '../errors/index.js';
+
 
 const register = async (req, res, next) => {
-        const user = await User.create(req.body);
-        res.status(201).json({ user });
+        const {name, password, email} = req.body
+        if ( !name || !password || !email){
+            throw new BadRequestError('please provide all the values')
+        }
+        const userAlreadyExists = await User.findOne({email});
+        if (userAlreadyExists){
+            throw new BadRequestError('Email Already In Use...')
+        }
+        const user = await User.create({ name, password, email });
+        res.status(StatusCodes.OK).json({ user });
 }
 
 const login = async (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,24 @@
 import { StatusCodes } from "http-status-codes";
+const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 const errorHandlerMiddleWare = (err, req, res, next) => {
     console.log(err)
+
     const defaultError = {
-        statusCode : StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: 'Something went wrong, tyr again later',
+        statusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, tyr again later',
     }
+    
     if (err.name==='ValidationError'){
         defaultError.statusCode = StatusCodes.BAD_REQUEST
         // defaultError.msg=err.message
         defaultError.msg = Object.values(err.errors).map((item)=> item.message).join(',')
+    }
+    if (err.code && err.code === 11000 ){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // defaultError.msg = `${capitalizeFirstLetter(Object.keys(err.keyValue))} field has to be unique`
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
     }
     // res.status(defaultError.statusCode).json({ msg: err })
     res.status(defaultError.statusCode).json({ msg: defaultError.msg })
